801d7f4aec0192bcafa892da7863e6816a1000e8
9names <60134748+9names@users.noreply.github.com>
2021-09-23 21:05:48 +1000

    Migrate i2c to newer alpha

--- src/i2c.rs
+++ src/i2c.rs
@@ -18,11 +18,14 @@
 */
 
 use bl602_pac::I2C;
-use embedded_hal::blocking;
-use embedded_hal::prelude::_embedded_hal_blocking_i2c_Read;
-use embedded_hal::prelude::_embedded_hal_blocking_i2c_Write;
+use embedded_hal::i2c::SevenBitAddress;
+// use embedded_hal::blocking;
+// use embedded_hal::prelude::_embedded_hal_blocking_i2c_Read;
+// use embedded_hal::prelude::_embedded_hal_blocking_i2c_Write;
 use embedded_hal_zero::blocking::i2c::Read as ReadZero;
 use embedded_hal_zero::blocking::i2c::Write as WriteZero;
+use embedded_hal::i2c;
+use embedded_hal::i2c::blocking;
 use embedded_time::rate::Hertz;
 
 use crate::{clock::Clocks, pac};
@@ -175,15 +178,15 @@ where
     }
 }
 
-impl<PINS> blocking::i2c::Read<blocking::i2c::SevenBitAddress> for I2c<pac::I2C, PINS>
+impl<PINS> i2c::blocking::Read<i2c::SevenBitAddress> for I2c<pac::I2C, PINS>
 where
     PINS: Pins<pac::I2C>,
 {
     type Error = Error;
 
-    fn try_read(
+    fn read(
         &mut self,
-        address: blocking::i2c::SevenBitAddress,
+        address: i2c::SevenBitAddress,
         buffer: &mut [u8],
     ) -> Result<(), Self::Error> {
         let fifo_config = self.i2c.i2c_fifo_config_0.read();
@@ -245,15 +248,15 @@ where
     }
 }
 
-impl<PINS> blocking::i2c::Write<blocking::i2c::SevenBitAddress> for I2c<pac::I2C, PINS>
+impl<PINS> i2c::blocking::Write<i2c::SevenBitAddress> for I2c<pac::I2C, PINS>
 where
     PINS: Pins<pac::I2C>,
 {
     type Error = Error;
 
-    fn try_write(
+    fn write(
         &mut self,
-        address: blocking::i2c::SevenBitAddress,
+        address: i2c::SevenBitAddress,
         buffer: &[u8],
     ) -> Result<(), Self::Error> {
         let fifo_config = self.i2c.i2c_fifo_config_0.read();
@@ -327,7 +330,7 @@ where
     type Error = Error;
 
     fn read(&mut self, address: u8, buffer: &mut [u8]) -> Result<(), Self::Error> {
-        self.try_read(address, buffer)
+        self.read(address, buffer)
     }
 }
 
@@ -338,6 +341,6 @@ where
     type Error = Error;
 
     fn write(&mut self, addr: u8, bytes: &[u8]) -> Result<(), Self::Error> {
-        self.try_write(addr, bytes)
+        self.write(addr, bytes)
     }
 }
